666a2ba26b66d07922c78607dd08c9ba14592e19
arai <arai_a@mac.com>
2018-10-26 14:12:33 +0900

    Add gather button

--- ditm-panel.js
+++ ditm-panel.js
@@ -12,12 +12,22 @@ content_field.placeholder = `1. Enter URL above
 3. Modify content
 4. Hit [Save]
 5. Load the webpage`;
+
 const url_field = document.getElementById("url");
 url_field.addEventListener("keypress", async event => {
-  if (event.key == "Enter") {
+  if (event.key === "Enter") {
     load();
   }
 });
+
+document.getElementById("save").addEventListener("click", save);
+document.getElementById("load").addEventListener("click", load);
+document.getElementById("remove").addEventListener("click", remove);
+document.getElementById("gather").addEventListener("click", gather);
+const stored_urls = document.getElementById("stored-urls");
+stored_urls.addEventListener("change", select_stored);
+const used_urls = document.getElementById("used-urls");
+used_urls.addEventListener("change", select_used);
 
 async function load() {
   browser.runtime.sendMessage({
@@ -42,11 +52,15 @@ async function remove() {
     url,
   });
 }
-async function select() {
+async function select_stored() {
   try {
     await files_promise;
 
     const url = stored_urls.value;
+    if (url === "---") {
+      return;
+    }
+    used_urls.value = "---";
     url_field.value = url;
     const file = files[stored_urls.value];
     content_field.value = file;
@@ -54,12 +68,125 @@ async function select() {
     console.log(e.toString());
   }
 }
+async function select_used() {
+  try {
+    const url = used_urls.value;
+    if (url === "---") {
+      return;
+    }
+    stored_urls.value = "---";
+    url_field.value = url;
+    await load();
+  } catch (e) {
+    console.log(e.toString());
+  }
+}
+async function gather() {
+  const [urls, error] = await browser.devtools.inspectedWindow.eval(`
+(function () {
+  const urls = [];
+  function gatherFromScript(script) {
+    const src = script.src;
+    if (src) {
+      urls.push(src)
+    }
+  }
+  function gatherFromCSSLink(link) {
+    const href = link.href;
+    const rel = link.rel;
+    if (rel === "stylesheet" && href) {
+      urls.push(href)
+    }
+  }
+  function gatherFromFrame(frame) {
+    const src = frame.src;
+    if (src) {
+      urls.push(src)
+    }
+    try {
+      gatherFromDocument(frame.contentDocument);
+    } catch (e) {
+    }
+  }
+  function gatherFromDocument(doc) {
+    const scripts = document.getElementsByTagName("script");
+    for (let i = 0; i < scripts.length; i++) {
+      gatherFromScript(scripts[i]);
+    }
+    const links = document.getElementsByTagName("link");
+    for (let i = 0; i < links.length; i++) {
+      gatherFromCSSLink(links[i]);
+    }
+    const frames = document.getElementsByTagName("frame");
+    for (let i = 0; i < frames.length; i++) {
+      gatherFromFrame(frames[i]);
+    }
+    const iframes = document.getElementsByTagName("iframe");
+    for (let i = 0; i < iframes.length; i++) {
+      gatherFromFrame(iframes[i]);
+    }
+  }
+  urls.push(document.location.href);
+  gatherFromDocument(document);
 
-document.getElementById("save").addEventListener("click", save);
-document.getElementById("load").addEventListener("click", load);
-document.getElementById("remove").addEventListener("click", remove);
-const stored_urls = document.getElementById("stored-urls");
-stored_urls.addEventListener("change", select);
+  return urls;
+})();
+`);
+  if (error) {
+    if ("isException" in error && error.isException) {
+      console.log("Exception: " + error.value);
+    }
+    else if ("isError" in error && error.isError) {
+      console.log("Error: " + error.code);
+    }
+    else {
+      console.log("Unknown error: " + error);
+    }
+    return;
+  }
+
+  fillUsedList(urls);
+}
+
+async function initList(list) {
+  while (list.firstChild) {
+    list.firstChild.remove();
+  }
+
+  const option = document.createElement("option");
+  option.value = "---";
+  option.textContent = "---";
+  list.appendChild(option);
+}
+
+async function fillStoredList() {
+  initList(stored_urls);
+
+  for (const url of Object.keys(files).sort()) {
+    const option = document.createElement("option");
+    option.value = url;
+    option.textContent = url;
+    stored_urls.appendChild(option);
+
+    if (needsContent) {
+      needsContent = false;
+      option.selected = true;
+      url_field.value = url;
+      content_field.value = files[url];
+    }
+  }
+}
+
+async function fillUsedList(urls) {
+  initList(used_urls);
+
+  for (const url of urls.sort()) {
+    const option = document.createElement("option");
+    option.value = url;
+    option.textContent = url;
+    used_urls.appendChild(option);
+  }
+}
 
 browser.runtime.onMessage.addListener(message => {
   switch (message.topic) {
@@ -68,24 +195,9 @@ browser.runtime.onMessage.addListener(message => {
       break;
     }
     case "list": {
-      while (stored_urls.firstChild) {
-        stored_urls.firstChild.remove();
-      }
       files = message.files;
       files_resolve(files);
-
-      for (const url of Object.keys(files).sort()) {
-        const option = document.createElement("option");
-        option.value = url;
-        option.textContent = url;
-        stored_urls.appendChild(option);
-
-        if (needsContent) {
-          needsContent = false;
-          url_field.value = url;
-          content_field.value = files[url];
-        }
-      }
+      fillStoredList();
       break;
     }
   }
