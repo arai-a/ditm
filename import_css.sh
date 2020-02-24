4866e826b4190aa2c51b107a494b37b57e1ccf93
Tooru Fujisawa <arai_a@mac.com>
2020-02-24 23:27:26 +0900

    Remove referece to browser resources

--- import_css.sh
+++ import_css.sh
@@ -6,16 +6,26 @@ if [ "x${M_C}" = "x" ]; then
 fi
 
 cp ${M_C}/devtools/client/themes/common.css devtools-common.css
-sed -i .bak -e 's|resource://devtools/client/themes/|devtools-|' devtools-common.css
-rm devtools-common.css.bak
+sed -i .bak -e 's|^@import\(.*\);$||' devtools-common.css
+sed -i .bak -e 's|chrome://devtools/skin/images/|./images/|' devtools-common.css
+sed -i .bak -e 's|chrome://devtools/skin/images/|./images/|' devtools-common.css
+sed -i .bak -e 's|chrome://browser/skin/|./images/|' devtools-common.css
 
 cp ${M_C}/devtools/client/themes/splitters.css devtools-splitters.css
 cp ${M_C}/devtools/client/themes/variables.css devtools-variables.css
+
 cp ${M_C}/devtools/client/themes/toolbars.css devtools-toolbars.css
+sed -i .bak -e 's|chrome://devtools/skin/images/|./images/|' devtools-toolbars.css
 
 cp ${M_C}/devtools/client/themes/tooltips.css devtools-tooltips.css
+sed -i .bak -e 's|chrome://global/skin/icons/|./images/|' devtools-tooltips.css
+sed -i .bak -e 's|chrome://devtools/skin/images/|./images/|' devtools-tooltips.css
+sed -i .bak -e 's|resource://devtools/client/shared/components/reps/images/|./images/|' devtools-tooltips.css
+sed -i .bak -e 's|^@import\(.*\);$||' devtools-tooltips.css
+
 
 cp ${M_C}/devtools/client/debugger/src/components/variables.css debugger-variables.css
 cp ${M_C}/devtools/client/debugger/src/components/Editor/Tabs.css debugger-Tabs.css
 cp ${M_C}/devtools/client/debugger/src/components/App.css debugger-App.css
 
+rm *.css.bak
